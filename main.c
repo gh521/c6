fd7ab8807b488999f08108ea485547029799be06
xx <qq@a.com>
2019-01-18 11:24:28 +0800

    结构框架

--- main.c
+++ main.c
@@ -1,9 +1,56 @@
 #include <stdio.h>
+struct student
+{
+    int yuwen;
+    int shuxue;
+    int yinyu;
+    char name[10010];
+};
 
-int main(){
+int main()
+{
 
     printf("xueshengchengjiguanlixitong\n");
 
-    return 0;
+    int index = 0;
+    while (1)
+    {
+        printf("1---tianjia\n");
+        printf("2---scanchu\n");
+        printf("3----xainshishuoyouxueshengchengji\n");
+        printf("4---zhongfenbujige\n");
+        printf("5---mriyikedoubujige\n");
+        printf("6-----xianshizongfenzuigaodexuesheng\n");
+        printf("7----xianshipinjunfenzuigaodexuesheng\n");
+        printf("8----tuichu\n");
+        int coude;
+        scanf("%d", &coude);
 
+        if (coude == 1)
+        {
+        }
+        if (coude == 2)
+        {
+        }
+        if (coude == 3)
+        {
+        }
+        if (coude == 4)
+        {
+        }
+        if (coude == 5)
+        {
+        }
+        if (coude == 6)
+        {
+        }
+        if (coude == 7)
+        {
+        }
+        if (coude == 8)
+        {
+        }
+
+        return 0;
+    }
 }
